e40f574fc3f87858f121dc7e7b8ef1a2740749e3
Priyansh Jain <Priyanshj1304@gmail.com>
2025-11-20 15:17:53 +0530

    feat: Add agents module structure

---
+++ agents/__init__.py
@@ -0,0 +1,10 @@
+"""CodeDebt Guardian Agents Module
+
+This module contains all agent implementations:
+- DebtDetectionAgent: Scans code for technical debt
+- PriorityRankingAgent: Scores and prioritizes debt items  
+- FixProposalAgent: Generates automated fix suggestions
+"""
+
+__version__ = "1.0.0"
+__author__ = "Priyansh Jain"
